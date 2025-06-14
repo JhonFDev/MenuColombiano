7e5a9809942e80f2b60598534f8980fb0d45a3d4
JhonFDev <jhonfdeveloper@gmail.com>
2025-06-14 18:31:13 -0500

    se agrego paper y se desinstalo elements, se configuro la caja de busquedas en Home.jsx, se agrego listado de CategoriesRecipes.jsx, was added dependencies expo-dev-client, expo-updates, vector-icons, se creo eas.json

--- components/Home/DishesCard.jsx
+++ components/Home/DishesCard.jsx
@@ -1,5 +1,4 @@
 import { Pressable, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
 import { useNavigation } from '@react-navigation/native'
 
 export default function DishesCard() {
--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,17 +1,62 @@
 import { View, Text, Image, ScrollView } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { TextInput, useTheme } from "react-native-paper";
 
 //mis Imports componentes necesarios
 import RecipesRow from "../components/Home/RecipesRow";
-
-
 
 const Home = () => {
   const insets = useSafeAreaInsets(); // Obteniendo los insets de la safe area
   return (
     <View style={{ paddingTop: insets.top, paddingBottom: insets.bottom }}>
+      {/* Barra buscador de recipientes */}
+      <View className="py-2 w-full px-4">
+        <View>
+          <TextInput
+            placeholder="Buscar recetas"
+            left={
+              <TextInput.Icon
+                icon="food-turkey"
+                color="black"
+                style={{ left: 20 }}
+              />
+            }
+            mode="outlined"
+            outlineColor="green"
+            activeOutlineColor="blue"
+            cursorColor="orange"
+            autoCapitalize="sentences"
+            autoCorrect={false}
+            keyboardType="default"
+            returnKeyType="search"
+            onSubmitEditing={() =>
+              alert("Buscando... y detruyendo al mundo! JAJAJA!")
+            }
+            clearButtonMode="always"
+            clearTextOnFocus={true}
+            underlineColorAndroid="green"
+            selectionColor="orange"
+            label={"¿Qué quieres preparar hoy?"}
+            theme={{
+              animation: {
+                scale: 2, // Aumenta la animación de escala
+              },
+              roundness: 20,
+            }}
+            style={{
+              height: 56,
+              fontSize: 16,
+              fontWeight: "500",
+              paddingHorizontal: 30,
+            }}
+          />
+        </View>
+      </View>
 
-      <ScrollView scrollEnabled={true} className="bg-amber-300 border-4 border-red-600">        
+      <ScrollView
+        scrollEnabled={true}
+        className="bg-amber-300 border-4 border-red-600"
+      >
         <Text className="text-2xl font-bold text-gray-900 mb-4 text-center tracking-widest pt-3">
           Home del menu colombiano
         </Text>
@@ -23,13 +68,7 @@ const Home = () => {
             <Text className="text-gray-500">30 min</Text>
             </View>
           </View> */}
-          <View>
-
-          {
-            <RecipesRow />
-          }
-
-          </View>
+        <View>{<RecipesRow />}</View>
       </ScrollView>
     </View>
   );
